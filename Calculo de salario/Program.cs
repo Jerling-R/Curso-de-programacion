a06412f5e1eef8b5cd6e780bedd0545bf0d76f0a
JERLING <JERLING@DESKTOP-3KGG2UN>
2025-03-04 20:26:10 -0400

    Agregar archivos de proyecto.

---
+++ Calculo de salario/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("!Ingrese su nombre!: ");
+String NombreEmpleado = Console.ReadLine();
+Console.WriteLine("!Ingrese su salario por hora!: ");
+decimal SalarioHora = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("!Ingrese la cantidad de horas trabajadas!: ");
+decimal HorasTrabajadas = Convert.ToDecimal(Console.ReadLine());
+
+static decimal SalarioBruto(decimal SalarioHora, decimal HorasTrabajadas)
+{
+    return SalarioHora * HorasTrabajadas;
+}
+
+decimal salarioBruto = SalarioBruto(SalarioHora, HorasTrabajadas);
+Console.WriteLine($@"
+El salario bruto de :        {NombreEmpleado} 
+es:                          {salarioBruto} 
+y las horas trabajadas son : {HorasTrabajadas}");
